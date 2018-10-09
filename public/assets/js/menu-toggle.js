9a8d9ae58522a97c0288cdb41021334b9cd277ee
tquoctuanit <tquoctuan97@gmail.com>
2018-10-09 10:21:05 +0700

    Add jquery-min and toggle open-info and close-info

--- public/assets/js/menu-toggle.js
+++ public/assets/js/menu-toggle.js
@@ -3,3 +3,11 @@ $(document).ready(function () {
         $(".grid-container").toggleClass("open-menu");
     })
 });
+
+function openInfo() {
+    $(".info-container").addClass("open-info");
+}
+
+function closeInfo() {
+    $(".info-container").removeClass("open-info");
+}
