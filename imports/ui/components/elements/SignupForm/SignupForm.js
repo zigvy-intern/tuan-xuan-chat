7e3c14e76ddeed84ca4adc14d1f45b19f53f0154
TuanTQ <tquoctuan97@gmail.com>
2018-11-01 15:26:16 +0700

    Confirm password and set avatar

--- imports/ui/components/elements/SignupForm/SignupForm.js
+++ imports/ui/components/elements/SignupForm/SignupForm.js
@@ -21,78 +21,80 @@ class SignupForm extends Component {
         e.preventDefault();
         let email = this.refs.email.value.trim(); 
         let password = this.refs.psw.value.trim();
+        let confirmPassword = this.refs.cfpsw.value.trim();
+        console.log('Password', password);
+        console.log('Confirm', confirmPassword);
         let id = this.refs.userID.value.trim();
         let name = this.refs.username.value.trim();
-        let color = this.refs.color.value.trim();
-
-        Accounts.createUser({
-            email,
-            password,
-            username: id,
-            password:password,
-            profile: { 
-                name,
-                color
-            }},(err) => {
-                this.setState({
-                    error: err
-                })
-          });
         
-        chatkit.createUser({
-            id: id,
-            name: name,
-            email:email,
-            customData: {
-              avatarColor: color,
-              role:"ctv"
-            },
-          })
-          .then(() => {
-            //Thông báo
-            alert("Successfully");
-            //Gán role
-            chatkit.assignGlobalRoleToUser({
-                userId: id,
-                roleName: 'admin',
-              })
-                .then(() => {
-                  
-                }).catch((err) => {
-                  console.log(err);
-                });
-          }).catch((err) => {
-            alert("Fail! "+err.error_description)
-            console.log(err);
-          });
-
+        if(password !== confirmPassword) {
+            this.setState({
+                error: 'Confirm password not match'
+            });
+        }else{
+            Accounts.createUser({
+                email,
+                password,
+                username: id,
+                password:password,
+                profile: { 
+                    name,
+                    color: 'blue'
+                }},(err) => {
+                    if(err) {
+                        this.setState({
+                          error: err.reason
+                        });
+                      }else{
+                        this.setState({
+                          error: ''
+                        });
+                      }
+            });
+            
+            chatkit.createUser({
+                id: id,
+                name: name,
+                email:email,
+                customData: {
+                avatarColor: 'blue',
+                role:"ctv"
+                },
+            })
+            .then(() => {
+                //Gán role
+                chatkit.assignGlobalRoleToUser({
+                    userId: id,
+                    roleName: 'admin',
+                })
+                    .then(() => {
+                    
+                    }).catch((err) => {
+                    console.log(err);
+                    });
+            });
+    }
   }
 
     render() {
-        console.log(chatkit.getUsers())
+        console.log(chatkit.getUsers());
+        let { error } = this.state;
         return (
             <LoginContainer>
                 <div className="login-form">
                     <img className="logo" src="./assets/img/SVG/logo.svg" alt="logodchat" />
                     <form method="POST" onSubmit={this.handleSubmit} ref="signUpForm">
                         <label htmlFor="userID">Your ID</label>
-                        <input className="text-box" type="text" name="userID" ref="userID" required  minLength={8} autoFocus autoComplete="off"  />
+                        <input className="text-box" type="text" name="userID" ref="userID" required  minLength={8} autoFocus  />
                         <label htmlFor="username">Display name</label>
-                        <input className="text-box" type="text" name="username" ref="username" maxLength={30} required autoComplete="off" />
+                        <input className="text-box" type="text" name="username" ref="username" maxLength={30} required />
                         <label htmlFor="psw">Password</label>
-                        <input className="text-box" type="password" name="psw" ref="psw" maxLength={20} required autoComplete="off" />
+                        <input className="text-box" type="password" name="psw" ref="psw" maxLength={20} required />
+                        <label htmlFor="psw">Confirm password</label>
+                        <input className="text-box" type="password" name="cfpsw" ref="cfpsw" maxLength={20} required />
                         <label htmlFor="email">Your email</label>
-                        <input className="text-box" type="email" name="email" ref="email" required autoComplete="off" />
-                        <label htmlFor="color">Choose your color</label>
-                        <select id="sel" name="color" ref="color">
-                            <option value="blue" className="bgr-blue">Blue</option>
-                            <option value="red" className="bgr-red">Red</option>
-                            <option value="pink" className="bgr-pink">Pink</option>
-                            <option value="yellow" className="bgr-yellow">Yellow</option>
-                            <option value="orange" className="bgr-orange">Orange</option>
-                            <option value="cyan" className="bgr-cyan">Cyan</option>
-                            <option value="green" className="bgr-green">Green</option>
-                        </select>
+                        <input className="text-box" type="email" name="email" ref="email" required />
+                        <p className="error">{error ? error : ''}</p>
                         <button className="btn btn--large btn--green" type="submit"> SIGN UP </button>
                         <div className="login--option">
                             <Link to={`/admin`} className="login__link">Back</Link>
